4ede3b16f052b9432e75df350aeeaf5b25060941
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-04 18:55:31 +0200

    update(form builder): only if theres item to show

--- app/components/shared/form_builder/Form_Builder.tsx
+++ app/components/shared/form_builder/Form_Builder.tsx
@@ -118,55 +118,61 @@ const Form_Builder = ({
   return (
     <>
       {formList?.length > 0 &&
-        formList?.map((formItem) => (
-          <fieldset
-            key={formItem.id}
-            className={`flex-1 grid gap-2 content-baseline ${formItem.containerClassName || ""}`}
-          >
-            {formItem.label && (
-              <label
-                className={`body text-neutral-black-600 capitalize font-medium ${formItem.labelClassName || ""}`}
-                htmlFor={formItem.fieldName}
-                dangerouslySetInnerHTML={{ __html: t(String(formItem.label)) }}
-              />
-            )}
-            {formItem.fieldName && (
-              <Controller
-                name={formItem.fieldName}
-                control={control}
-                rules={formItem.validator}
-                render={({ field, fieldState: { error } }) =>
-                  renderField(formItem, field, error)
-                }
-              />
-            )}
-            {/* info */}
-            {formItem.info && !errors?.[formItem.fieldName]?.message && (
-              <p className="text-neutral-white-100 body">{formItem.info}</p>
-            )}
-            {errors?.[formItem.fieldName]?.message &&
-              !formItem?.inlineError && (
-                <p className="flex items-center gap-1">
-                  <span>
-                    <InfoIcon
-                      width="20"
-                      height="20"
-                      fill={
-                        formItem?.errorFill ?? "var(--color-semantic-red-900)"
-                      }
-                    />
-                  </span>
-                  <span
-                    className={`text-semantic-red-900 text-xs ${
-                      formItem?.errorClassName ?? ""
-                    }`}
-                  >
-                    {t(String(errors[formItem.fieldName]?.message))}
-                  </span>
-                </p>
-              )}
-          </fieldset>
-        ))}
+        formList?.map(
+          (formItem) =>
+            formItem && (
+              <fieldset
+                key={formItem.id}
+                className={`flex-1 grid gap-2 content-baseline ${formItem.containerClassName || ""}`}
+              >
+                {formItem.label && (
+                  <label
+                    className={`body text-neutral-black-600 capitalize font-medium ${formItem.labelClassName || ""}`}
+                    htmlFor={formItem.fieldName}
+                    dangerouslySetInnerHTML={{
+                      __html: t(String(formItem.label)),
+                    }}
+                  />
+                )}
+                {formItem.fieldName && (
+                  <Controller
+                    name={formItem.fieldName}
+                    control={control}
+                    rules={formItem.validator}
+                    render={({ field, fieldState: { error } }) =>
+                      renderField(formItem, field, error)
+                    }
+                  />
+                )}
+                {/* info */}
+                {formItem.info && !errors?.[formItem.fieldName]?.message && (
+                  <p className="text-neutral-white-100 body">{formItem.info}</p>
+                )}
+                {errors?.[formItem.fieldName]?.message &&
+                  !formItem?.inlineError && (
+                    <p className="flex items-center gap-1">
+                      <span>
+                        <InfoIcon
+                          width="20"
+                          height="20"
+                          fill={
+                            formItem?.errorFill ??
+                            "var(--color-semantic-red-900)"
+                          }
+                        />
+                      </span>
+                      <span
+                        className={`text-semantic-red-900 text-xs ${
+                          formItem?.errorClassName ?? ""
+                        }`}
+                      >
+                        {t(String(errors[formItem.fieldName]?.message))}
+                      </span>
+                    </p>
+                  )}
+              </fieldset>
+            )
+        )}
     </>
   );
 };
