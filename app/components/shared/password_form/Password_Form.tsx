78df81908a3129fa1edf25c7ee1f4d590911a440
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-04 18:55:57 +0200

    update(password):seperate into component 2 be able to use it many times

---
+++ app/components/shared/password_form/Password_Form.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
+import type { FormListItemType } from "../form_builder/Form_Builder-types";
+import { passwordPattern } from "~/common/constant/validator";
+import { handleError } from "~/common/utils/handleError";
+import Form_Builder from "../form_builder/Form_Builder";
+import Button from "../button/Button";
+
+type PasswordFormProps = {
+  isRest?: boolean;
+};
+const Password_Form = ({ isRest = true }: PasswordFormProps) => {
+  const { t } = useTranslation();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  // ___________ useform _________
+  const {
+    control,
+    setError,
+    getValues,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      password: null,
+      confirmPassword: null,
+    },
+    mode: "onChange",
+  });
+  // _______________ list ______________
+  const formList: FormListItemType[] | null[] = [
+    !isRest
+      ? {
+          id: "1",
+          formType: "password",
+          name: "old_password",
+          label: "old_password",
+          fieldName: "old_password",
+          validator: {
+            required: "old_password_is_required",
+            pattern: {
+              value: passwordPattern,
+              message: "password_pattern_error",
+            },
+          },
+          showForgetPassword: false,
+          inlineError: false,
+        }
+      : null,
+    {
+      id: "1",
+      formType: "password",
+      name: "password",
+      label: "new_password",
+      fieldName: "password",
+      validator: {
+        required: "password_is_required",
+        pattern: {
+          value: passwordPattern,
+          message: "password_pattern_error",
+        },
+      },
+      showForgetPassword: false,
+      inlineError: false,
+    },
+    {
+      id: "2",
+      formType: "password",
+      name: "confirmPassword",
+      label: "confirmPassword",
+      fieldName: "confirmPassword",
+      validator: {
+        required: "confirm_password_is_required",
+        validate: (value: string) => {
+          const password = getValues("password");
+          return value === password || "password_mismatch";
+        },
+      },
+      showForgetPassword: false,
+      inlineError: false,
+    },
+  ];
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      navigate(`/`);
+    } catch (err) {
+      handleError(err, t, setError);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="px-4 flex flex-col gap-10"
+    >
+      <fieldset className="flex flex-col gap-4">
+        <Form_Builder formList={formList} control={control} errors={errors} />
+      </fieldset>
+
+      <Button
+        loading={loading}
+        disabled={loading}
+        text={isRest ? "reset_password" : "change_password"}
+        type="submit"
+        hasFullWidth={isRest}
+      />
+    </form>
+  );
+};
+
+export default Password_Form;
--- app/routes/auth/Reset_Password.tsx
+++ app/routes/auth/Reset_Password.tsx
@@ -9,60 +9,11 @@ import Form_Builder from "~/components/shared/form_builder/Form_Builder";
 import type { FormListItemType } from "~/components/shared/form_builder/Form_Builder-types";
 import Page_Header from "~/components/shared/header/page_header/Page_Header";
 import type { breadCrumbListType } from "~/components/shared/header/page_header/Page_Header.types";
+import Password_Form from "~/components/shared/password_form/Password_Form";
 
 const Reset_Password = () => {
   const { t } = useTranslation();
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  // ___________ useform _________
-  const {
-    control,
-    setError,
-    getValues,
-    formState: { errors, isValid },
-    handleSubmit,
-  } = useForm({
-    defaultValues: {
-      password: null,
-      confirmPassword: null,
-    },
-    mode: "onChange",
-  });
-  // _______________ list ______________
-  const formList: FormListItemType[] = [
-    {
-      id: "1",
-      formType: "password",
-      name: "password",
-      label: "new_password",
-      fieldName: "password",
-      validator: {
-        required: "password_is_required",
-        pattern: {
-          value: passwordPattern,
-          message: "password_pattern_error",
-        },
-      },
-      showForgetPassword: false,
-      inlineError: false,
-    },
-    {
-      id: "2",
-      formType: "password",
-      name: "confirmPassword",
-      label: "confirmPassword",
-      fieldName: "confirmPassword",
-      validator: {
-        required: "confirm_password_is_required",
-        validate: (value: string) => {
-          const password = getValues("password");
-          return value === password || "password_mismatch";
-        },
-      },
-      showForgetPassword: false,
-      inlineError: false,
-    },
-  ];
+
   const breadcrumbsList: breadCrumbListType[] = [
     {
       label: t("login"),
@@ -74,17 +25,7 @@ const Reset_Password = () => {
       label: t("reset_password"),
     },
   ];
-  // _________________function __________-
-  const onSubmit = async (data) => {
-    try {
-      setLoading(true);
-      navigate(`/`);
-    } catch (err) {
-      handleError(err, t);
-    } finally {
-      setLoading(false);
-    }
-  };
+
   return (
     <main className="flex flex-col gap-20">
       <Page_Header
@@ -94,26 +35,7 @@ const Reset_Password = () => {
       />
       <section className="container">
         <div className=" max-w-[400px] sm:max-w-[320px] flex flex-col mx-auto w-full gap-8">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="px-4 flex flex-col gap-10"
-          >
-            <fieldset className="flex flex-col gap-6">
-              <Form_Builder
-                formList={formList}
-                control={control}
-                errors={errors}
-              />
-            </fieldset>
-
-            <Button
-              loading={loading}
-              disabled={loading || !isValid}
-              text="reset_password"
-              type="submit"
-              hasFullWidth
-            />
-          </form>
+          <Password_Form />
         </div>
       </section>
     </main>
