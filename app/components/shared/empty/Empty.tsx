9f90b6b29d3c710df865230ada68d1b256788f18
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-05 13:25:28 +0200

    update: show title

--- app/components/shared/empty/Empty.tsx
+++ app/components/shared/empty/Empty.tsx
@@ -15,9 +15,11 @@ const Empty = ({
 }: EmptyPropsType) => {
   const { t } = useTranslation();
   return (
-    <figure className="flex flex-col gap-6">
+    <figure className="flex flex-col items-center justify-center text-center gap-6 max-w-[273px] w-full mx-auto ">
       <img src={image} alt="empty" className="w-16 h-16" />
-      {title && <h3 className="h3 font-bold text-neutral-black-900"></h3>}
+      {title && (
+        <h3 className="h3 font-bold text-neutral-black-900">{t(title)}</h3>
+      )}
       {des && <p className="body text-neutral-black-500">{t(des)}</p>}
       {btnName && (
         <Button
