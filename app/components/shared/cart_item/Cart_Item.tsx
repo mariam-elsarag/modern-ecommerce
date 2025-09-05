cfbee0a22c056a42db9cd71fc449074cb827827c
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-05 13:12:09 +0200

    update(cart): Add variant to make it cover alot of places like profile order and wishlist

--- app/components/shared/cart_item/Cart_Item.tsx
+++ app/components/shared/cart_item/Cart_Item.tsx
@@ -5,13 +5,84 @@ import { formatPrice } from "~/common/utils/formatPrice";
 import Counter from "../counter/Counter";
 import Button from "../button/Button";
 import { CloseIcon } from "~/assets/icons/Icon";
+import { data, Link } from "react-router";
+import { formatDateToMonth } from "~/common/utils/formatDateToMonth";
+import axiosInstance from "~/services/axiosInstance";
+import { handleError } from "~/common/utils/handleError";
 
 type CartItemProps = {
   product: CartProductsType;
+  variant?: "cart" | "wishlist" | "order";
 };
-const Cart_Item = ({ product }: CartItemProps) => {
+const Cart_Item = ({ product, variant = "cart" }: CartItemProps) => {
   const { t } = useTranslation();
   console.log(product, "s");
+  const toggleFavorite = async () => {
+    try {
+      // const response=await axiosInstance.patch()
+    } catch (err) {
+      handleError(err, t);
+    }
+  };
+
+  const addToCart = async () => {
+    try {
+      // const respones=await axiosInstance.post()
+    } catch (err) {
+      handleError(err, t);
+    }
+  };
+
+  const renderRightContent = () => {
+    switch (variant) {
+      case "wishlist":
+        return (
+          <>
+            <span className="text-neutral-black-900 body font-medium">
+              {product?.price ? formatPrice(product?.price) : "0"}
+            </span>
+            <Button
+              text="add_to_cart"
+              variant="outline_dark"
+              handleClick={addToCart}
+            />
+          </>
+        );
+      case "order":
+        return (
+          <>
+            <Link
+              className="text-neutral-black-900 body font-medium border-b border-neutral-black-900"
+              to={`/${product?.orderId}/checkout`}
+            >
+              {t("proceessing")}
+            </Link>
+            <Button
+              text="view_item"
+              variant="outline_dark"
+              to={`/product/${product?.id}`}
+            />
+          </>
+        );
+      default:
+        return (
+          <>
+            <Counter quantity={product?.quantity} />
+            <Button
+              size="md"
+              variant="secondary"
+              icon={<CloseIcon fill="var(--color-neutral-black-500)" />}
+            />
+          </>
+        );
+    }
+  };
+  const containerBase = `flex-1 flex  `;
+  const containerStyle = {
+    cart: "items-center justify-between",
+    order: "flex-col gap-2",
+    wishlist: "flex-col gap-2",
+  };
   return (
     <section className="flex gap-8 sm:items-center flex-col sm:flex-row">
       <figure className="w-full h-[250px] sm:w-20 sm:h-20 rounded-[4px] bg-neutral-white-100 flex items-center justify-center">
@@ -23,48 +94,61 @@ const Cart_Item = ({ product }: CartItemProps) => {
           />
         )}
       </figure>
-      <div className="flex-1 flex items-center justify-between">
+      <div className={`${containerBase} ${containerStyle[variant]} `}>
         <div className="flex flex-col gap-2">
           <h3 className="body font-medium text-neutral-black-900 line-clamp-1">
             {product?.title}
           </h3>
-          <div className="flex items-center gap-2">
-            {product?.color && (
-              <div className="flex items-center gap-2">
-                <span className="text-xs text-neutral-black-500 font-medium">
-                  {t("color")}
-                </span>
+          {variant === "cart" ? (
+            <div className="flex items-center gap-2">
+              {product?.color && (
+                <div className="flex items-center gap-2">
+                  <span className="text-xs text-neutral-black-500 font-medium">
+                    {t("color")}
+                  </span>
 
-                <span
-                  className="flex items-center justify-center w-3 h-3 border border-neutral-white-200 rounded-full"
-                  style={{ background: product?.color }}
-                />
-              </div>
-            )}
-            {product?.size && (
-              <div className="flex items-center gap-2">
-                <span className="text-neutral-black-500">—</span>
-                <p className="flex items-center gap-1 text-neutral-black-500 text-xs font-medium">
-                  <span>{t("size")}:</span>
-                  <span>{product?.size}</span>
-                </p>
-              </div>
-            )}
-          </div>
+                  <span
+                    className="flex items-center justify-center w-3 h-3 border border-neutral-white-200 rounded-full"
+                    style={{ background: product?.color }}
+                  />
+                </div>
+              )}
+              {product?.size && (
+                <div className="flex items-center gap-2">
+                  <span className="text-neutral-black-500">—</span>
+                  <p className="flex items-center gap-1 text-neutral-black-500 text-xs font-medium">
+                    <span>{t("size")}:</span>
+                    <span>{product?.size}</span>
+                  </p>
+                </div>
+              )}
+            </div>
+          ) : (
+            <p className="flex items-center gap-1 label font-medium text-neutral-black-500">
+              <span>{t(variant === "order" ? "ordered_on" : "added_on")}</span>
+              <span>
+                {product?.createdAt
+                  ? formatDateToMonth(product?.createdAt)
+                  : "-"}
+              </span>
+            </p>
+          )}
         </div>
-        <span className="text-neutral-black-900 body font-medium">
-          {product?.price ? formatPrice(product?.price) : "0"}
-        </span>
+        {variant === "wishlist" ? (
+          <span
+            className="label text-neutral-black-900 font-medium cursor-pointer"
+            onClick={toggleFavorite}
+          >
+            {t("remove_item")}
+          </span>
+        ) : (
+          <span className="text-neutral-black-900 body font-medium">
+            {product?.price ? formatPrice(product?.price) : "0"}
+          </span>
+        )}
       </div>
       {/* remove and quantity */}
-      <div className="flex items-center gap-4 ">
-        <Counter quantity={product?.quantity} />
-        <Button
-          size="md"
-          variant="secondary"
-          icon={<CloseIcon fill="var(--color-neutral-black-500)" />}
-        />
-      </div>
+      <div className="flex items-center gap-4 ">{renderRightContent()}</div>
     </section>
   );
 };
