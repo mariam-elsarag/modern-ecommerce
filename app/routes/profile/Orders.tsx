c72eba03e805744af7a6770f70a02c9d1219b94f
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-05 13:14:45 +0200

    feat(orders):Create order page

--- app/routes/profile/Orders.tsx
+++ app/routes/profile/Orders.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import Cart_Item from "~/components/shared/cart_item/Cart_Item";
+import Pagination from "~/components/shared/pagination/Pagination";
+import useGetData from "~/hooks/useGetData";
+import { API } from "~/services/apiUrl";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { t } = useTranslation();
+  const { data } = useGetData(API.products);
+  return (
+    <div className="max-w-[617px] flex flex-col gap-10 ">
+      <h2 className="h5 font-semibold text-neutral-black-900">{t("orders")}</h2>
+      <div className="flex flex-col gap-10">
+        <section className="flex-1 flex flex-col gap-8 ">
+          {data?.map((cart) => (
+            <div
+              className="pb-8 border-b border-neutral-white-200"
+              key={cart?.id}
+            >
+              <Cart_Item product={cart} variant="order" />
+            </div>
+          ))}
+        </section>
+        <div className="flex items-center justify-center">
+          <Pagination />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
