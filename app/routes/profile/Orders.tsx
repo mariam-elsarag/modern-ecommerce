d90dd8eb5e102f29232efe79ab1bad32c585f65e
mariam-elsarag <mariamelsarag44@gmail.com>
2025-09-05 13:25:11 +0200

    update(empty):Add empty

--- app/routes/profile/Orders.tsx
+++ app/routes/profile/Orders.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
 import Cart_Item from "~/components/shared/cart_item/Cart_Item";
+import Empty from "~/components/shared/empty/Empty";
 import Pagination from "~/components/shared/pagination/Pagination";
 import useGetData from "~/hooks/useGetData";
 import { API } from "~/services/apiUrl";
 
 const Orders = () => {
   const { t } = useTranslation();
-  const { data } = useGetData(API.products);
+  const navigate = useNavigate();
+  const data = [];
+  if (data?.length === 0) {
+    return (
+      <div className="h-full flex flex-col justify-center items-center">
+        <Empty
+          des="empty_order"
+          btnName="start_shopping"
+          btnCta={() => {
+            navigate("/product");
+          }}
+        />
+      </div>
+    );
+  }
   return (
-    <div className="max-w-[617px] flex flex-col gap-10 ">
+    <div className={`max-w-[617px] flex flex-col gap-10`}>
       <h2 className="h5 font-semibold text-neutral-black-900">{t("orders")}</h2>
       <div className="flex flex-col gap-10">
         <section className="flex-1 flex flex-col gap-8 ">
@@ -22,9 +38,11 @@ const Orders = () => {
             </div>
           ))}
         </section>
-        <div className="flex items-center justify-center">
-          <Pagination />
-        </div>
+        {data?.length > 0 && (
+          <div className="flex items-center justify-center">
+            <Pagination />
+          </div>
+        )}
       </div>
     </div>
   );
